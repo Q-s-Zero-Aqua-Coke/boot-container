588f95b27e647a534724ac2d0106cc429875941c
LeeWonGyu <bitamin707@naver.com>
2025-05-21 10:45:17 +0900

    초기 설정 추가

---
+++ src/main/java/org/example/bootcontainer/container/TestController.java
@@ -0,0 +1,16 @@
+package org.example.bootcontainer.container;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello World");
+    }
+}
