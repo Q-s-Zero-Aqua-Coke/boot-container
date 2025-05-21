159cde0f4ff71ae808e3ebe779a53f04b2908501
LeeWonGyu <bitamin707@naver.com>
2025-05-21 11:14:20 +0900

    Spring 기본 구조 추가

--- src/main/java/org/example/bootcontainer/container/TestController.java
+++ src/main/java/org/example/bootcontainer/container/TestController.java
@@ -1,16 +1,39 @@
 package org.example.bootcontainer.container;
 
+import lombok.RequiredArgsConstructor;
+import org.example.bootcontainer.dto.AnimalDTO;
+import org.example.bootcontainer.entity.Animal;
+import org.example.bootcontainer.repository.AnimalRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class TestController {
+
+    private final AnimalRepository animalRepository;
 
     @GetMapping
     public ResponseEntity<String> hello() {
         return ResponseEntity.ok("Hello World");
+    }
+
+    @GetMapping("/animals")
+    public ResponseEntity<List<AnimalDTO>> getAnimal() {
+        return ResponseEntity.ok(
+                animalRepository.findAll().stream().map(
+                        el -> new AnimalDTO(el.getName())
+                ).toList());
+    }
+
+    @PostMapping("/animals")
+    public ResponseEntity<Animal> addAnimal(@RequestBody AnimalDTO animalDTO) {
+        Animal animal = new Animal();
+        animal.setName(animalDTO.name());
+        animalRepository.save(animal);
+        return ResponseEntity.ok(animal);
     }
 }
